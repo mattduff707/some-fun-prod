21b465ccedbe264adadffe1f155f919728d04b19
Matthew Duffy <mattduff707@hotmail.com>
2023-07-31 09:29:10 -0700

    squiggle shadow and center

--- src/app/General.jsx
+++ src/app/General.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const General = () => {
   return (
-    <section className="bg-seaweed h-[100vh] max-w-full overflow-hidden relative">
-      <div className="w-full h-[72px] bg-pinky absolute bottom-0"></div>
+    <section className="bg-seaweed h-[100vh] max-w-full overflow-hidden relative flex flex-col items-center">
+      <div className="w-full h-[82px] bg-pinky absolute bottom-0"></div>
       <div
         alt=""
-        className="absolute bottom-0 inline-block w-[1920px] h-[136px] bg-greenSquiggle bg-auto bg-center"
+        className="absolute bottom-0 inline-block w-[1920px] h-[142px] bg-greenSquiggle bg-auto bg-center"
       />
     </section>
   );
--- src/app/Stack.jsx
+++ src/app/Stack.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Stack = () => {
   return (
-    <section className="bg-pinky h-[100vh] max-w-full overflow-hidden relative">
-      <div className="w-full h-[72px] bg-darkDijon absolute bottom-0"></div>
+    <section className="bg-pinky h-[100vh] max-w-full overflow-hidden relative flex flex-col items-center">
+      <div className="w-full h-[76px] bg-darkDijon absolute bottom-0"></div>
       <div
         alt=""
         className="absolute bottom-0 inline-block w-[1920px] h-[136px] bg-pinkSquiggle bg-auto bg-center"
