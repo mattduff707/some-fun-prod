2b3c4a3af664b721d3604685528845b895fcfe03
Matthew Duffy <51209857+mattduff707@users.noreply.github.com>
2023-08-02 16:04:55 -0700

    general WIP

--- src/app/General.jsx
+++ src/app/General.jsx
@@ -1,29 +1,85 @@
-import Box from '@/components/Box';
-import BoxBtn from '@/components/BoxBtn';
-import React from 'react';
+import Box from "@/components/Box";
+import BoxBtn from "@/components/BoxBtn";
+import Image from "next/image";
+import React from "react";
+import alissaPic from "../assets/alissaPic.png";
+import mattPic from "../assets/mattPic.png";
+import Highlight from "@/components/Highlight";
+import Star from "@/components/Star";
 
 const General = () => {
   return (
-    <section className="bg-seaweed h-[100vh] max-w-full overflow-hidden relative flex flex-col items-center">
-      <div className="max-w-constraint pt-10">
-        <Box boxClass={'shadow-seaweedShadow border-seaweed bg-pale pt-10 px-12 pb-14 relative'}>
-          <h2 className="text-burntDark font-bold text-xl">Howdy and Welcome!</h2>
+    <section className="bg-seaweed max-w-full overflow-hidden relative flex flex-col items-center pb-[212px] pt-[24px]">
+      <div className="max-w-[1000px] w-full pt-10 flex flex-col items-start isolate">
+        <Box
+          boxClass={
+            "shadow-seaweedShadow border-seaweed bg-pale pt-10 px-12 pb-14 relative z-10"
+          }
+        >
+          <h2 className="text-burntDark font-bold text-xl">
+            Howdy and Welcome!
+          </h2>
           <p className="text-lg text-seaweed max-w-[425px] pt-4">
-            Some Fun Studio is a distinguished boutique creative studio dedicated to catering to the design and
-            programming needs of small businesses and startups in various industries.
+            Some Fun Studio is a distinguished boutique creative studio
+            dedicated to catering to the design and programming needs of small
+            businesses and startups in various industries.
           </p>
           <div className="absolute bottom-[-26px] left-[50%] -translate-x-[50%]">
             <BoxBtn
-              buttonClass={'border-4 border-seaweed px-12 py-3 font-semibold text-seaweed bg-pale'}
-              shadowClass={'bg-burnt'}
+              buttonClass={
+                "border-4 border-seaweed px-12 py-3 font-semibold text-seaweed bg-pale"
+              }
+              shadowClass={"bg-burnt"}
             >
               START A PROJECT
             </BoxBtn>
           </div>
         </Box>
+        <div className="flex w-full justify-center">
+          <div className="relative translate-y-[140px] translate-x-[16px]">
+            <Star
+              className={
+                "w-[180px] h-[180px] absolute bottom-[-40px] left-[-60px] z-10"
+              }
+            />
+            <Image
+              src={alissaPic}
+              alt="Alissa holding a paint up when she was a child"
+              className="border-[15px] border-pale rounded-[30px] w-[420px] h-auto shadow-seaweedShadow shadow-box origin-bottom-left rotate-[-10deg]"
+            />
+          </div>
+          <div className="relative translate-y-[-120px] z-20">
+            <Star
+              className={
+                "w-[180px] h-[180px] absolute top-[-20px] right-[-60px] z-10"
+              }
+            />
+            <Image
+              src={mattPic}
+              alt="Matt on a computer playing a game when he was a child"
+              className="border-[15px] border-pale rounded-[30px] w-[420px] h-auto shadow-seaweedShadow shadow-box origin-top-left rotate-[9deg]"
+            />
+          </div>
+        </div>
+        <Box
+          boxClass={
+            "shadow-seaweedShadow border-seaweed bg-pale py-[60px] px-12 relative self-end"
+          }
+        >
+          <p className="text-lg text-seaweed max-w-[425px] pt-4">
+            We are <Highlight>creative</Highlight>,{" "}
+            <Highlight>curious</Highlight>, and <Highlight>holistic</Highlight>{" "}
+            in our thinking. We embrace the process. Every new project is an
+            opportunity to <Highlight>learn</Highlight>,{" "}
+            <Highlight>explore</Highlight>, and <Highlight>grow</Highlight>.
+          </p>
+        </Box>
       </div>
       <div className="w-full h-[82px] bg-pinky absolute bottom-0"></div>
-      <div alt="" className="absolute bottom-0 inline-block w-[1920px] h-[142px] bg-greenSquiggle bg-auto bg-center" />
+      <div
+        alt=""
+        className="absolute bottom-0 inline-block w-[1920px] h-[142px] bg-greenSquiggle bg-auto bg-center"
+      />
     </section>
   );
 };
---
+++ src/components/Highlight.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Highlight = ({ children }) => {
+  return <span className="font-bold text-burntDark">{children}</span>;
+};
+
+export default Highlight;
