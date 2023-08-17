eaada3526925cf9cad5773236b66ecb68f2b8c9d
Matthew Duffy <mattduff707@hotmail.com>
2023-08-17 09:38:54 -0700

    about page responsive

--- src/app/About.jsx
+++ src/app/About.jsx
@@ -12,18 +12,18 @@ import Smiley from "@/components/icons/Smiley";
 
 const About = () => {
   return (
-    <section className="px-section relative flex max-w-full flex-col items-center overflow-hidden bg-seawater bg-squiggleBg bg-cover pt-8">
+    <section className="relative flex max-w-full flex-col items-center overflow-hidden bg-seawater bg-squiggleBg bg-cover px-section pt-8">
       <div className="flex w-full flex-col items-center  pb-[240px]">
-        <div className="w-full max-w-constraint pt-pageTop">
+        <div className="w-full max-w-constraint pt-pageTop max1000:flex max1000:flex-col max1000:items-center">
           <Title
             boxClass="border-seaweed bg-pale shadow-pinkyShadow shadow-seaweedShadow mb-16"
             titleClass={"fill-seaweed translate-x-[24px] translate-y-[4px]"}
           >
             THE TEAM
           </Title>
-          <div className="flex  items-center">
+          <div className="flex items-center max1000:flex-col max1000:gap-16">
             <Image
-              className="h-auto w-[50%] origin-top-right translate-y-12 rotate-[-7deg] rounded-[60px] border-[15px] border-seaweed shadow-box shadow-seaweedShadow"
+              className="h-auto w-[50%] origin-top-right translate-y-12 rotate-[-7deg] rounded-[60px] border-[15px] border-seaweed shadow-box shadow-seaweedShadow max1000:w-full max1000:translate-y-0 max1000:rotate-0"
               width={1001}
               height={1291}
               src={usPic}
@@ -31,7 +31,7 @@ const About = () => {
             />
             <Box
               boxClass={
-                "shadow-seaweedShadow border-seaweed bg-pale pt-10 px-12 pb-14 relative z-10 w-[50%]"
+                "shadow-seaweedShadow border-seaweed bg-pale pt-10 px-12 pb-14 relative z-10 w-[50%] max1000:w-full"
               }
             >
               <p className="max-w-[425px] pt-4 text-lg text-seaweed">
